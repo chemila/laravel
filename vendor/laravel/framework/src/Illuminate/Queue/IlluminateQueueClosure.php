6c5bf5f1272cafe5688a484a69a4c1498be8aa93
chemila <chemila@me.com>
2015-02-27 21:13:26 +0800

    update

---
+++ vendor/laravel/framework/src/Illuminate/Queue/IlluminateQueueClosure.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Encryption\Encrypter;
+
+class IlluminateQueueClosure {
+
+	/**
+	 * The encrypter instance.
+	 *
+	 * @var \Illuminate\Encryption\Encrypter  $crypt
+	 */
+	protected $crypt;
+
+	/**
+	 * Create a new queued Closure job.
+	 *
+	 * @param  \Illuminate\Encryption\Encrypter  $crypt
+	 * @return void
+	 */
+	public function __construct(Encrypter $crypt)
+	{
+		$this->crypt = $crypt;
+	}
+
+	/**
+	 * Fire the Closure based queue job.
+	 *
+	 * @param  \Illuminate\Queue\Jobs\Job  $job
+	 * @param  array  $data
+	 * @return void
+	 */
+	public function fire($job, $data)
+	{
+        /** @var closure $closure */
+		$closure = unserialize($this->crypt->decrypt($data['closure']));
+
+		$closure($job);
+	}
+
+}
